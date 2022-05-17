d6ab37905994b8e114a702a6eab4148fd677ccb3
Vladislav <vladik.morozov.2012@mail.ru>
2022-05-17 13:50:05 +1000

    Update gameplay balance.

--- Assets/LevelControllers/Scripts/BattleCityScript.cs
+++ Assets/LevelControllers/Scripts/BattleCityScript.cs
@@ -22,8 +22,9 @@ namespace SimpleVoxelTanks.LevelControllers
         public const int TeamsCount = 2;
         public static readonly int[] BasesCount = { 1, 0 };
         public static readonly Vector3UInt Size = new(21, 2, 21);
-        public uint EnemyTickets { get; protected set; } = 4;
+        public uint EnemyTickets { get; protected set; } = 12;
         public uint LiveEnemy { get; protected set; } = 0;
+        public uint MaxLiveEnemies { get; protected set; } = 4;
         public WASDTankController PlayerController { get; protected set; }
 
         private void OnPlayerDied ()
@@ -52,6 +53,9 @@ namespace SimpleVoxelTanks.LevelControllers
             {
                 foreach (var _ in MapBuilder.Teams[1].SpawnPoints)
                 {
+                    if (LiveEnemy >= MaxLiveEnemies)
+                        break;
+
                     var tank = SpawnEnemyTank(Random.Range(0, 2) is 0 || PlayerController.TankDiscreteModel == null
                         ? targetBase
                         : PlayerController.TankDiscreteModel);
@@ -62,7 +66,7 @@ namespace SimpleVoxelTanks.LevelControllers
                     if (EnemyTickets == 0)
                         break;
                 }
-                yield return new WaitForSeconds(30f);
+                yield return new WaitForSeconds(20f);
             }
         }
 
